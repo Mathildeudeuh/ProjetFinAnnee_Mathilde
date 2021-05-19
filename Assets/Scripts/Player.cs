3d3837b859ec40e0911485dbf8796849afa2af7c
Mathilde D <m.degris@tudiant.ecolemeilies.fr>
2021-05-19 10:14:19 +0200

    Create Script Input Action
    + add var controls to script player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    // Variable pour utiliser les contôles
+    private Controls controls;
+
     // Start is called before the first frame update
     void Start()
     {
