57ddc03152a2dade1adaa0dc8fdec7aa764e964e
Mathilde D <m.degris@tudiant.ecolemeilies.fr>
2021-05-25 13:02:52 +0200

    Rename Class Time --> Timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Time : MonoBehaviour
+public class Timer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
