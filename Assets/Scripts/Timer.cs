62aa7d0b46841dc1dc8439f046fa05cd39cc2c25
Mathilde D <m.degris@tudiant.ecolemeilies.fr>
2021-05-25 18:59:54 +0200

    Add UI Script

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] private float seconde;
+    public Text myText;
+
+    void Start()
+    {
+        
+    }
 
     void Update()
     {
         if (seconde > 0)
         {
             seconde -= Time.deltaTime;
+            myText.text = Mathf.Round(seconde).ToString();
             Debug.Log(seconde);
         }
     }
